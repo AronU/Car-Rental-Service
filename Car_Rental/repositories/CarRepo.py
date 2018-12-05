98b0dba5089980a416fec1129e7a38186b53cf54
Aron <aronu97@hotmail.com>
2018-12-05 22:30:21 +0000

    Fixed intent issues

--- Car_Rental/repositories/CarRepo.py
+++ Car_Rental/repositories/CarRepo.py
@@ -8,27 +8,22 @@ class CarRepository:
         self.__available_cars = []
 
     def get_available_cars(self):
-    '''Reads the data file. Splits each line up into the cars
-        attributes, although we only care about availability this time.
-        If the availability is 1, then it puts it into the list. This list
-        will therefore be containing all available cars. -Aron 05/12/2018'''
-
-        with open("./data/cars.csv") as car_file:
+        '''Reads the data file. Splits each line up into the cars attributes,
+        although we only care about availability this time. If the availability
+        is 1, then it puts it into the list. This list will therefore be 
+        containing all available cars. -Aron'''
+        with open("./data/cars.csv", "r") as car_file:
             csv_reader = csv.reader(car_file)
             next(csv_reader)
             for line in csv_reader:
                 #licence_plate, brand, model, year, availability = line.split(",")
                 if line[3] == 1:
                     self.__available_cars.append(line)    
-        
-        return self.__available_cars
+            return self.__available_cars
 
-    def get_unavailable_cars(self):
-    '''Reads the data file. Splits each line up into the cars
-        attributes, although we only care about availability this time.
-        If the availability is 0, then it puts it into the list. This list
-        will therefore be containing all unavailable cars. -Aron 05/12'''
-
+    """ def get_unavailable_cars(self):
+    '''This function works the same as get_available_cars above but does it for 
+    unavailable cars instead - Aron'''
         with open("./data/Cars.txt", "r") as car_file:
             for line in car_file.readlines():
                 licence_plate, brand, model, year, availability = line.split(",")
@@ -36,4 +31,4 @@ class CarRepository:
                     unavailable_car = Car(licence_plate, brand, model, year, availability)
                     self.__unavailable_cars.append(unavailable_car)    
         
-        return self.__unavailable_cars
+        return self.__unavailable_cars """
