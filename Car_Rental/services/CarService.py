c0d56d41d50570dd4fa570e8dc6c3e19fb1ad03f
AronU <aronu97@hotmail.com>
2018-12-05 16:45:50 +0000

    Unavailable cars implemented

--- Car_Rental/services/CarService.py
+++ Car_Rental/services/CarService.py
@@ -10,3 +10,10 @@ class CarService:
            from there. - Aron 05/12'''
         available_car_list = self.__car_repo.get_available_cars()
         return available_car_list
+
+    def get_unavailable_cars(self):
+        '''This function is pulled from the UI and is supposed to 
+       go into the Car Repository and get the unavailable car list 
+       from there. - Aron 05/12'''
+        unavailable_car_list = self.__car_repo.get_unavailable_cars()
+        return unavailable_car_list
