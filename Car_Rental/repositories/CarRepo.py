db06999fe629b36f915db9befd1f453e805b1770
AronU <aronu97@hotmail.com>
2018-12-06 13:25:45 +0000

    Fixed the functionality in avail and unavail, learn to count Aron

--- Car_Rental/repositories/CarRepo.py
+++ Car_Rental/repositories/CarRepo.py
@@ -6,6 +6,7 @@ class CarRepository:
     def __init__(self):
         #self.__cars = []
         self.__available_cars = []
+        self.__unavailable_cars = []
 
     def get_available_cars(self):
         '''Reads the data file. Splits each line up into the cars attributes,
@@ -16,19 +17,20 @@ class CarRepository:
             csv_reader = csv.reader(car_file)
             next(csv_reader)
             for line in csv_reader:
-                #licence_plate, brand, model, year, availability = line.split(",")
-                if line[3] == 1:
+                if line[4] == "1":
                     self.__available_cars.append(line)    
             return self.__available_cars
 
-    """ def get_unavailable_cars(self):
-    '''This function works the same as get_available_cars above but does it for 
-    unavailable cars instead - Aron'''
-        with open("./data/Cars.txt", "r") as car_file:
-            for line in car_file.readlines():
-                licence_plate, brand, model, year, availability = line.split(",")
-                if availability == 0:
-                    unavailable_car = Car(licence_plate, brand, model, year, availability)
-                    self.__unavailable_cars.append(unavailable_car)    
-        
-        return self.__unavailable_cars """
+    def get_unavailable_cars(self):
+        '''Reads the data file. Splits each line up into the cars attributes,
+        although we only care about availability this time. If the availability
+        is 0, then it puts it into the list. This list will therefore be 
+        containing all unavailable cars. -Aron'''
+        with open("./data/cars.csv", "r") as car_file:
+            csv_reader = csv.reader(car_file)
+            next(csv_reader)
+            for line in csv_reader:
+                if line[4] == "0":
+                    self.__unavailable_cars.append(line)    
+            return self.__unavailable_cars
+
