3e0145cb5b593ad936e343da442542a203b5eee6
AronU <aronu97@hotmail.com>
2018-12-12 16:23:21 +0000

    changed list in get all cars

--- Car_Rental/repositories/CarRepo.py
+++ Car_Rental/repositories/CarRepo.py
@@ -52,12 +52,12 @@ class CarRepository:
         '''Reads the data file. splits each line up into the cars attributes,
         and returns all cars regardless of availability.'''
         with open("./data/cars.csv", "r") as car_file:
-            self.__all_cars = []
+            all_cars = []
             csv_reader = csv.reader(car_file)
             next(csv_reader)
             for line in csv_reader:
-                self.__all_cars.append(line)
-            return self.__all_cars
+                all_cars.append(line)
+            return all_cars
 
     
     def return_car(self, licence_plate):
