c750190c3fa5bc04e749bfd564b21851236d4264
andrilor <Andri.lorenz@gmail.com>
2018-12-12 18:41:25 +0000

    Order_menu komið

--- Car_Rental/ui/Order_menu.py
+++ Car_Rental/ui/Order_menu.py
@@ -9,11 +9,7 @@ from services.CustomerService import CustomerService
 # Gets access to the CarService class in the service folder
 from services.CarService import CarService
 
-from ui.Search_order_ssn import search_order_ssn
-import ui.Search_order_ssn as search_ssn
-
-from ui.Search_order_licence_plate import search_order_licence_plate
-import ui.Search_order_licence_plate as search_licence_p
+from services.OrderService import OrderService
 
 # constant variables to be used to take in commands through the input
 Back_1 = "b"
@@ -21,17 +17,49 @@ Back_2 = "back"
 Home_1 = "m"
 Home_2 = "main menu"
 
+def order_list_printer(order_list, lp_or_ssn=1):
+    if lp_or_ssn == 1:
+        print("The order where this user: {} whas involved: \n".format(order_list[0][3]))
+
+        for line in order_list:
+            print("ID: {} - Licence plate: {} - Start date: {} - End date: {} - Payment way: {} - Additional insurence: {}\n".format(line[0], line[1], line[4], line[5], line[6], line[7]))
+        un_func.printline()
+        input("Press Enter to coninue: ")
+        un_func.printline()
+    elif lp_or_ssn == 2:
+        print("The order where {} whas rented out: \n".format(order_list[0][1]))
+        for line in order_list:
+            print("ID: {} - SSN: {} - Name: {} - Start date: {} - End date: {} - Payment way: {}- Additional insurence: {}\n".format(line[0], line[2], line[3], line[4], line[5], line[6], line[7]))
+        un_func.printline()
+        input("Press Enter to coninue: ")
+        un_func.printline()
+    return False
+
 def Search_order_menu():
+    order_service = OrderService()
+    customer_service = CustomerService()
     count = True
     while count == True:
         print("You can search for an order from user's SSN or by rental car licence plate.\n")
-        print("1.  Search by SSN\n2.  Search by licence plate\n")
         un_func.printer()
         Choice = input("Choice: ").lower()
         un_func.printline()
         if Choice == Home_1 or Choice == Home_2 or Choice == Back_1 or Choice == Back_2:
             count = False
-        elif Choice == "1":
-            count = search_ssn.search_order_ssn()
-        elif Choice == "2":
-            count = search_licence_p.search_order_licence_plate()
+        elif Choice == "":
+            print("\nERROR: You have to search for something\n")
+            un_func.printline()
+        elif Choice.isdigit() == True:
+            if customer_service.valid_customer_check(Choice) == True:
+                order_ssn_list = order_service.get_order_ssn(Choice)
+                order_list_printer(order_ssn_list)
+                order_ssn_list.clear()
+        elif Choice.isdigit() == False:
+            Choice = Choice.upper()
+            order_licence_plate_list = order_service.get_order_licence_plate(Choice)
+            order_list_printer(order_licence_plate_list, 2)
+            order_licence_plate_list.clear()
+        elif Choice.isdigit() == False:
+            print("Please enter a valid licence plate\n")
+        else:
+            print("Order does not exist\n")
